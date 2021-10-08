70256bd284378fa014f07ece75d8d90464c09af5
胡瑞林 <huruilin@wondersgroup.com>
2021-10-08 10:04:32 +0800

    引入tsx语法

--- src/components/client/Copyright.vue
+++ src/components/client/Copyright.vue
@@ -1,16 +1,23 @@
-<template>
-  <div class="copyright-wrapper">
-    <p class="copyright-content">蜀ICP备20021607号-1</p>
-  </div>
-</template>
 
-<script>
-export default {}
+<script lang="tsx">
+import { ref, defineComponent } from "vue";
+export default defineComponent({
+  setup(props, ctx) {
+    const copyright =ref(`蜀ICP备20021607号-1`) ;
+    return () => {
+      return (
+        <div class="copyright-wrapper">
+          <p class="copyright-content">{copyright.value}</p>
+        </div>
+      );
+    };
+  },
+});
 </script>
 <style scoped lang="scss">
 .copyright-content {
   line-height: 40px;
   letter-spacing: 2px;
-  @include font(12px,#ccc,center);
+  @include font(12px, #ccc, center);
 }
 </style>
